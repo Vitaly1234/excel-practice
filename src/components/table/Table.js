70ebd3d1682e728c1a4c00fa17a6021d8b9ee0ee
Vitaly Pomykalov <pomykalovvm@mail.ru>
2021-04-25 17:58:56 +0400

    select cells logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,14 @@
+import {
+  getMatrix,
+  isCell,
+  shouldResize,
+} from '@/components/table/table.functions';
+
+import {$} from '@core/dom';
 import {ExcelComponent} from '@core/ExcelComponent';
+import {TableSelection} from '@/components/table/TableSelection';
 import {createTable} from '@/components/table/table.template';
 import {resizeHandler} from '@/components/table/table.resize';
-import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -17,9 +24,31 @@ export class Table extends ExcelComponent {
     return createTable(100);
   }
 
+  prepare() {}
+
+  init() {
+    super.init();
+
+    this.selection = new TableSelection();
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selection.select($cell);
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      const isCtrlDown = event.ctrlKey;
+      if (event.shiftKey) {
+        const $cells = getMatrix($target, this.selection.selected).map((id) =>
+          this.$root.find(`[data-id="${id}"]`)
+        );
+
+        this.selection.selectGroup($cells, isCtrlDown);
+      } else {
+        this.selection.select($target, isCtrlDown);
+      }
     }
   }
 }
---
+++ src/components/table/TableSelection.js
@@ -0,0 +1,33 @@
+export class TableSelection {
+  static className = 'selected';
+
+  constructor() {
+    this.group = [];
+    this.selected = null;
+  }
+
+  select($el, isUnion) {
+    if (!isUnion) {
+      this.clear();
+    }
+
+    $el.addClass(TableSelection.className);
+    this.group.push($el);
+    this.selected = $el;
+  }
+
+  clear() {
+    this.group.forEach(($el) => $el.removeClass(TableSelection.className));
+    this.group = [];
+  }
+
+  selectGroup($group = [], isUnion) {
+    if (!isUnion) {
+      this.clear();
+      this.group = $group;
+    } else {
+      this.group = [...this.group, ...$group];
+    }
+    this.group.forEach(($el) => $el.addClass(TableSelection.className));
+  }
+}
