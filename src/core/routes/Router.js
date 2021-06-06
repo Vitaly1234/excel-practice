ca1ce87de37a153b5a15e5e36a8203bb45970c9d
Vitaly Pomykalov <pomykalovvm@mail.ru>
2021-06-06 22:19:24 +0400

    add router + dashboard page

---
+++ src/core/routes/Router.js
@@ -0,0 +1,45 @@
+import {$} from '@core/dom';
+import {ActiveRoute} from '@core/routes/ActiveRoute';
+
+export class Router {
+  constructor(selector, routes) {
+    if (!selector) {
+      throw new Error('Selector is required');
+    }
+
+    this.$placeholder = $(selector);
+    this.routes = routes;
+
+    this.page = null;
+
+    this.changePageHandler = this.changePageHandler.bind(this);
+
+    this.init();
+  }
+
+  init() {
+    window.addEventListener('hashchange', this.changePageHandler);
+    this.changePageHandler();
+  }
+
+  changePageHandler() {
+    if (this.page) {
+      this.page.destroy();
+    }
+
+    this.$placeholder.clear();
+
+    const params = ActiveRoute.param;
+    const route = params[0];
+    const Page = this.routes[route] || this.routes['dashboard'];
+
+    this.page = new Page(params);
+    this.$placeholder.append(this.page.getRoot());
+
+    this.page.afterRender();
+  }
+
+  destroy() {
+    window.removeEventListener('hashchange', this.changePageHandler);
+  }
+}
---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,36 @@
+import {$} from '@core/dom';
+import {Page} from '@core/Page';
+
+export class DashboardPage extends Page {
+  getRoot() {
+    return $.create('div', 'db').html(`
+    <div class="db__header">
+          <h1>Excel Dashboard</h1>
+        </div>
+        <div class="db__new">
+          <div class="db__view">
+            <a href="#" class="db__create">
+              Новая <br />
+              Таблица
+            </a>
+          </div>
+        </div>
+        <div class="db__table db__view">
+          <div class="db__list-header">
+            <span>Название</span>
+            <span>Дата открытия</span>
+          </div>
+          <ul class="db__list">
+            <li class="db__record">
+              <a href="#">Таблица номер 1</a>
+              <strong>08.04.2021</strong>
+            </li>
+
+            <li class="db__record">
+              <a href="#">Таблица номер 2</a>
+              <strong>08.04.2021</strong>
+            </li>
+          </ul>
+        </div>`);
+  }
+}
